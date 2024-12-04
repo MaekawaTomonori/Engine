f066043ee68d8907c382166e1ecbded470714e78
Apocalypse <k023g0063@g.neec.ac.jp>
2024-12-04 16:06:30 +0900

    Assets(リネーム済み)の追加

---
+++ Project/Assets/Shaders/Particle.PS.hlsl
@@ -0,0 +1,28 @@
+﻿#include "Particle.hlsli"
+
+struct Material{
+    float32_t4 color;
+    uint32_t enableLighting;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+
+Texture2D gTexture : register(t0);
+SamplerState gSampler : register(t0);
+
+struct PixelShaderOutput{
+    float32_t4 color : SV_TARGET0;
+};
+
+//PixelShaderOutput main(GeometryShaderOutput input) {
+PixelShaderOutput main(VertexShaderOutput input) {
+    PixelShaderOutput output;
+    float32_t4 texColor = gTexture.Sample(gSampler, input.texcoord);
+
+    if (texColor.a == 0.f) {discard;}
+
+    output.color = gMaterial.color * texColor * input.color;
+
+    if (output.color.a == 0.f){discard;}
+
+    return output;
+}
---
+++ Project/Assets/Shaders/Particle.VS.hlsl
@@ -0,0 +1,23 @@
+﻿#include "Particle.hlsli"
+
+struct ParticleForGPU{
+    float32_t4x4 WVP;
+    float32_t4x4 World;
+    float32_t4 color;
+};
+StructuredBuffer<ParticleForGPU> gParticles : register(t0);
+
+struct VertexShaderInput{
+	float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
+};
+
+VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID) {
+    VertexShaderOutput output;
+    output.position = mul(input.position, gParticles[instanceId].WVP);
+    output.texcoord = input.texcoord;
+    output.color = gParticles[instanceId].color;
+
+    return output;
+}
