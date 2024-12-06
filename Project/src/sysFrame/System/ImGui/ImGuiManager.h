fb1c90a21922143de08111124667916e970944a9
Apocalypse <k023g0063@g.neec.ac.jp>
2024-12-06 23:57:16 +0900

    リンカエラーを修正

--- Project/src/sysFrame/System/ImGui/ImGuiManager.h
+++ Project/src/sysFrame/System/ImGui/ImGuiManager.h
@@ -7,9 +7,9 @@ class SRVManager;
 class WinApp;
 class DirectXCommon;
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_dx12.h"
-#include "imgui/imgui_impl_win32.h"
+#include "imgui.h"
+#include "imgui_impl_dx12.h"
+#include "imgui_impl_win32.h"
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class ImGuiManager{
